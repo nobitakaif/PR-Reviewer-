0c8589504538e99d5189b6f642a85fa16bb5e953
mohd kaif <mk2818356@gmail.com>
2026-04-06 00:44:11 +0530

    updated signin page

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -3,46 +3,46 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
+import { motion } from "motion/react"
 import { signIn } from "@/utils/auth-client"
 import { useState } from "react"
 import { FaGithub } from "react-icons/fa"
 
 
-export default function Signin(){
+export default function Signin() {
 
     const [loading, setLoading] = useState(false)
-    
-    const handleGithubSignin = async () =>{
+
+    const handleGithubSignin = async () => {
         setLoading(true)
         await signIn.social({
-            provider : "github",
-            callbackURL : "/repos"
+            provider: "github",
+            callbackURL: "/repos"
         })
         setLoading(false)
     }
-    
+
     return <div className="flex justify-center items-center min-h-screen ">
-        <Card className="w-full max-w-md">
+        <Card className="w-full max-w-md  ring-0 shadow-xl border-2 border-gray-300">
             <CardHeader className="text-center">
                 <CardTitle className="text-center font-bold text-xl">Signin</CardTitle>
                 <CardDescription className="text-slate-400">Sign-in with your email or GitHub account</CardDescription>
             </CardHeader>
             <CardContent>
-                <Button className="" variant={"outline"} disabled={loading} onClick={handleGithubSignin}>
-                    <FaGithub/>Sign in with GitHub 
-                </Button>
-                <div>
-                    <div>
-                        <Separator className="w-full"/>
-                    </div>
-                    <div>
-                        or signin with email
-                    </div>
+                <motion.button className="w-full border-gray-300 flex  items-center justify-center gap-2 border rounded-lg p-1.5" disabled={loading} onClick={handleGithubSignin} animate={{}} whileHover={{ scale: 1.03 }} initial={{}}>
+                    <FaGithub />Sign in with GitHub
+                </motion.button>
+                <div className="relative flex items-center my-4">
+                    <div className="h-px bg-gray-300 flex-1"></div>
+
+                    <span className="mx-2 text-xs uppercase text-muted-foreground bg-background px-2 whitespace-nowrap">
+                        or sign in with email
+                    </span>
+
+                    <div className="h-px bg-gray-400 flex-1"></div>
                 </div>
             </CardContent>
-            <form className="m-2">
-                <Input placeholder="enter your email "/>
-            </form>
+
         </Card>
     </div>
 }
