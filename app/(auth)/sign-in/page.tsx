680a4ebdb45304c83b7ecdf1b490d7093c31a6e3
mohd kaif <mk2818356@gmail.com>
2026-04-06 03:01:44 +0530

    added db and fix signin endpoint

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,12 +1,14 @@
 "use client"
 import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Separator } from "@/components/ui/separator"
-import { motion } from "motion/react"
-import { signIn } from "@/utils/auth-client"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { useForm } from "@tanstack/react-form"
+import { convertOffsetToTimes, motion } from "motion/react"
+import { signIn, signUp} from "@/utils/auth-client"
 import { useState } from "react"
 import { FaGithub } from "react-icons/fa"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { FieldInfo } from "@/components/fieldInfo"
 
 
 export default function Signin() {
@@ -21,6 +23,26 @@ export default function Signin() {
         })
         setLoading(false)
     }
+
+    const form = useForm({
+        defaultValues: {
+            email: '',
+            password: ''
+        },
+        onSubmit: async ({ value }) => {
+            console.log(value.email, "   ", value.password)
+            await signUp.email({ 
+                email : value.email,
+                name : "Mohd Kaif", 
+                password : value.password
+             })
+            console.log("user is logged successfully!")
+            alert("alright")
+            form.reset()
+        }
+    })
+
+
 
     return <div className="flex justify-center items-center min-h-screen ">
         <Card className="w-full max-w-md  ring-0 shadow-xl border-2 border-gray-300">
@@ -41,7 +63,58 @@ export default function Signin() {
 
                     <div className="h-px bg-gray-400 flex-1"></div>
                 </div>
+                <div>
+                    <form onSubmit={(e) => {
+                        e.preventDefault()
+                        e.stopPropagation()
+                        form.handleSubmit()
+                    }}>
+                        <div className="flex flex-col gap-3 w-full">
+                            <form.Field
+                                name="email"
+                                validators={({
+                                    onChange: ({ value }) => !value && "email must be required" 
+                                })}
+                                children={(field) => {
+                                    return (
+                                        <>
+                                            <Label htmlFor={field.name}>Email</Label>
+                                            <Input id={field.name} name={field.name} value={field.state.value} onChange={(e) => field.handleChange(e.target.value)} placeholder="email@gmail.com" className="focus-visible:ring-1 focus-visible:ring-offset-0 border-gray-300" />
+                                            <FieldInfo field={field}/>
+                                        </>
+                                    )
+                                }}
+                            />
+                            <form.Field 
+                                name="password"
+                                validators={({
+                                    onChange : ({value }) => !value ? "password must be required" : value.length < 7 && "password is too short" 
+                                })}
+                                children={(field) =>{
+                                    return (
+                                        <>
+                                            <Label htmlFor={field.name}>Password</Label>
+                                            <Input id={field.name} name={field.name} value={field.state.value} placeholder="*********" type="password" onChange={(e)=>{
+                                                field.handleChange(e.target.value) 
+                                            }} className="focus-visible:ring-1 border-gray-300 focus-visible:ring-offset-0"/>
+                                            <FieldInfo field={field}/>
+                                        </>
+                                    )
+                                }}
+                            />
+                            <form.Subscribe 
+                                selector={(state)=> [state.canSubmit, state.isSubmitting]}
+                                children={([cansubmit, isSubmitting])=> (
+                                    <>
+                                    <Button type="submit" disabled={!cansubmit} variant={"outline"}> {isSubmitting ? "..." : "SignIn"}</Button>
+                                    </>
+                                )}
+                            />
+                        </div>
+                    </form>
+                </div>
             </CardContent>
+
 
         </Card>
     </div>
---
+++ components/fieldInfo.tsx
@@ -0,0 +1,12 @@
+import type { AnyFieldApi } from '@tanstack/react-form'
+
+export function FieldInfo({ field }: { field: AnyFieldApi }) {
+  return (
+    <>
+      {field.state.meta.isTouched && !field.state.meta.isValid ? (
+        <em className='text-red-500'>{field.state.meta.errors.join(',')}</em>
+      ) : null}
+      {field.state.meta.isValidating ? 'Validating...' : null}
+    </>
+  )
+}
