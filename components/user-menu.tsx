9aa979f90ec23b82c2c22cf590e03ca063df7d88
mohd kaif <mk2818356@gmail.com>
2026-04-07 05:04:42 +0530

    updated user-menu

--- components/user-menu.tsx
+++ components/user-menu.tsx
@@ -1,8 +1,10 @@
 import { signOut } from "@/utils/auth-client"
 import { useRouter } from "next/navigation"
-import { DropdownMenuTrigger, DropdownMenu } from "./ui/dropdown-menu"
+import { DropdownMenuTrigger, DropdownMenu, DropdownMenuContent, DropdownMenuSeparator, DropdownMenuItem } from "./ui/dropdown-menu"
 import { Button } from "./ui/button"
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
+import { ChevronDownCircleIcon, Settings, User } from "lucide-react"
+import { FaChevronCircleDown } from "react-icons/fa"
 
 interface UserProps{
     id : string,
@@ -23,12 +25,38 @@ export function UserMenu({user} : {user: UserProps}){
     
     return <DropdownMenu>
         <DropdownMenuTrigger asChild>
-            <Button variant={"ghost"} className="h-9 gap-2 px-2 hover:bg-muted/80">
-                <Avatar>
+            <Button variant={"ghost"} className="h-9 gap-2 px-2 hover:bg-muted/80 hover:bg-gray-300/30 py-2 ">
+                <Avatar className="size-7 ring-1 ring-border bg-gray-300/60">
                     <AvatarImage src={user.image ?? undefined} alt={user.name ?? "User"}/>
-                    <AvatarFallback>{firstWordOfName}</AvatarFallback>
+                    <AvatarFallback className="text-transform:capitalize">{firstWordOfName}</AvatarFallback>
                 </Avatar>
+                <span className="sm:inline-block text-sm font-medium text-transform:uppercase max-w25 truncate">{user.name ?? "User"}</span>
+                <FaChevronCircleDown className="hidden sm:inline-block " />
             </Button>
         </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-64">
+            <div className="px-3 py-3">
+                <div className="flex items-center gap-3">
+                    <Avatar className="size-10 ring-1 ring-border">
+                        <AvatarImage src={user.image ?? undefined} alt={user.name ?? "User"} />
+                        <AvatarFallback className="text-sm font-medium bg-gray-300/60 text-gray-800">
+                            {firstWordOfName}
+                        </AvatarFallback>
+                    </Avatar>
+                    <div className="flex flex-col min-w-9">
+                            <span className="text-sm font-medium truncate">{user.name ?? "User"}</span>
+                            <span className="text-xs text-gray-500 truncate">{user.email ?? "Email"}</span>
+                        </div>
+                </div>
+            </div>
+            <DropdownMenuSeparator/>
+            <DropdownMenuItem className="gap-2 py-2 cursor-pointer hover:bg-gray-300/30" disabled>
+                <User className="size-4"/> Profile
+            </DropdownMenuItem>
+            <DropdownMenuItem className="gap-2 py-2 cursor-pointer hover:bg-gray-300/30" disabled>
+                <Settings className="size-4"/>
+                Settings
+            </DropdownMenuItem>
+        </DropdownMenuContent>
     </DropdownMenu>
 }
